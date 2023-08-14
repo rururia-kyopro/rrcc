a881a5dcadf9195b0267a557fa8f6df9f2f1ac54
rururia-kyopro <130348385+rururia-kyopro@users.noreply.github.com>
2023-08-14 21:22:27 +0900

    tokenizer, parser

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,223 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+bool consume(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+void expect(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op)
+        error_at(token->str, "Not '%c'", op);
+    token = token->next;
+}
+
+int expect_number() {
+    if(token->kind != TK_NUM)
+        error_at(token->str, "Not a number");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str){
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token *tokenize(char *p){
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while(*p){
+        if(isspace(*p)){
+            p++;
+            continue;
+        }
+
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if(isdigit(*p)){
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error_at(p, "Tokenize error");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
+
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary();
+Node *mul();
+
+Node *expr() {
+    Node *node = mul();
+
+    for(;;){
+        if(consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if(consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for(;;){
+        if(consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if(consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary() {
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+    switch(node->kind){
+        case ND_ADD:
+            gen(node->lhs);
+            gen(node->rhs);
+            printf("  pop rsi\n");
+            printf("  pop rdi\n");
+            printf("  add rdi,rsi\n");
+            printf("  push rdi\n");
+            break;
+        case ND_SUB:
+            gen(node->lhs);
+            gen(node->rhs);
+            printf("  pop rsi\n");
+            printf("  pop rdi\n");
+            printf("  sub rdi,rsi\n");
+            printf("  push rdi\n");
+            break;
+        case ND_MUL:
+            gen(node->lhs);
+            gen(node->rhs);
+            printf("  pop rsi\n");
+            printf("  pop rax\n");
+            printf("  cqto\n");
+            printf("  mul rsi\n");
+            printf("  push rax\n");
+            break;
+        case ND_DIV:
+            gen(node->lhs);
+            gen(node->rhs);
+            printf("  pop rsi\n");
+            printf("  pop rax\n");
+            printf("  cqto\n");
+            printf("  div rsi\n");
+            printf("  push rax\n");
+            break;
+        case ND_NUM:
+            printf("  push %d\n", node->val);
+            break;
+    }
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,30 +225,17 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  user_input = argv[1];
+  token = tokenize(user_input);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
+  gen(node);
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
