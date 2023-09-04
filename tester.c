505982c6520db44edc22d073ea6f5c1054c4b90d
rururia-kyopro <130348385+rururia-kyopro@users.noreply.github.com>
2023-09-04 13:18:29 +0900

    Test: switch default statement.

--- tester.c
+++ tester.c
@@ -340,6 +340,7 @@ int main() {
     assert_file(2, "int main() {switch(2) {case 2: return 2; case 1: return 1; } return 0;}");
     assert_file(3, "int main() {int a=0;switch(2) {case 3: a+=3; case 2: a+=2; case 1: a+=1; } return a;}");
     assert_file(0, "int main() {int a=0;switch(4) {case 3: a+=3; case 2: a+=2; case 1: a+=1; } return a;}");
+    assert_file(1, "int main() {int a=0;switch(4) {case 3: a+=3; case 2: a+=2; default: case 1: a+=1; } return a;}");
     printf("OK\n");
     return 0;
 }
