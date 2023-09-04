005692087ec98ce026b8bff8775f7e80c5438ed2
rururia-kyopro <130348385+rururia-kyopro@users.noreply.github.com>
2023-09-04 09:44:06 +0900

    Test: Add sizeof array type.

--- tester.c
+++ tester.c
@@ -322,6 +322,8 @@ int main() {
     assert_file(2, "int main() {int v[2];v[0]=1;v[1]=2;int *a;a=v;a++;return *a;}");
     assert_file(10, "struct A{int a;struct {int b;};};int main() {struct A f;f.b=10;f.a=9;return f.b;}");
     assert_file(2, "int main() {char a[10-sizeof(int*)];return sizeof(a);}");
+    assert_file(12, "int main() {return sizeof(int [3]);}");
+    assert_file(24, "int main() {return sizeof(int *[3]);}");
     printf("OK\n");
     return 0;
 }
