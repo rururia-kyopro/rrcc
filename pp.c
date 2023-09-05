6dda48522e7233c63d14a6d5c64829f0d3f33e44
rururia-kyopro <130348385+rururia-kyopro@users.noreply.github.com>
2023-09-05 20:20:32 +0900

    PP: Fix bug that #elif are met even if preceded if/ifdef was met.

--- pp.c
+++ pp.c
@@ -664,6 +664,8 @@ static PPToken *if_group(PPToken **cur) {
                 if(!any_met && met) {
                     if_condition_met = true;
                     any_met = true;
+                }else {
+                    if_condition_met = false;
                 }
                 continue;
             } else if(pp_consume(cur, "else")) {
